58ef554d88c9cdc84fc49ad5b980ce99f8214bef
Mark Wagh <mark.wagh20@gmail.com>
2023-12-17 21:56:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,79 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secret key of your choice
 
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
+# Flask-Login setup
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+# User class for Flask-Login
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+
+# Replace this with your user authentication logic
+def authenticate(username, password):
+    # Example: check username and password against a database
+    if username == 'user' and password == 'password':
+        return User(1)  # User with ID 1
+    return None
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+# Define the login form
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+# Route for the login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = authenticate(username, password)
+
+        if user:
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+
+    return render_template('login.html', form=form)
+
+
+# Route for the dashboard (example of a protected route)
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return f'Hello, {current_user.id}! This is your dashboard.'
+
+
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
